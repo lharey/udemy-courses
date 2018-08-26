9bfff4bcf301cf1fbc62b265c419979e0fa2d1c0
Lisa Hare <lharey@gmail.com>
2018-08-26 16:08:09 +0100

    Upto end of section 3 starting assignement 1

--- react-the-complete-guide-incl-redux/react-complete-guide/src/App.js
+++ react-the-complete-guide-incl-redux/react-complete-guide/src/App.js
@@ -3,17 +3,72 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi I'm a react App</h1>
-        <Person />
-      </div>
+    state = {
+        persons: [
+            { name: 'Alex', age: 4 },
+            { name: 'Erin', age: 6 },
+            { name: 'Archie', age: 0 },
+        ]
+    }
 
-    );
+    switchNameHandler = (newName) => {
+        this.setState({
+            persons: [
+                { name: newName, age: 4 },
+                { name: 'Erin', age: 6 },
+                { name: 'Archie', age: 0 },
+            ]
+        });
+    }
 
-    //return React.createElement('div',{ className: "App" },React.createElement('h1',null,"Hi I'm a react App!!!!"));
-  }
+    nameChangedHandler = (event) => {
+        this.setState({
+            persons: [
+                { name: 'Alex', age: 4 },
+                { name: event.target.value, age: 6 },
+                { name: 'Archie', age: 0 },
+            ]
+        });
+    }
+
+    render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
+        return (
+            <div className="App">
+                <h1>Hi I'm a react App</h1>
+                <button
+                    onClick={() => this.switchNameHandler('Alexander')}
+                    style={style}
+                >Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
+                    clickNameHandler={this.switchNameHandler.bind(this, 'Alexander!')}
+                />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    changed={this.nameChangedHandler}
+                >
+                    My hobbies are drawing
+                </Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                />
+            </div>
+
+        );
+
+        //return React.createElement('div',{ className: "App" },React.createElement('h1',null,"Hi I'm a react App!!!!"));
+    }
 }
 
 export default App;
--- react-the-complete-guide-incl-redux/react-complete-guide/src/Person/Person.js
+++ react-the-complete-guide-incl-redux/react-complete-guide/src/Person/Person.js
@@ -1,9 +1,30 @@
+// import React, { Component } from 'react';
 import React from 'react';
 
+import './Person.css';
 
-const Person = () => {
+// class Person extends Component {
+//     render () {
+//         return (
+//             <div>
+//                 <p>My name is {this.props.name} and I'm {this.props.age} years old.</p>
+//                 <p>{this.props.children}</p>
+//             </div>
+//         );
+//     }
+// }
+
+const Person = (props) => {
     return (
-        <p>I'm a person</p>
+        <div className="Person">
+            <p onClick={props.clickNameHandler}>My name is {props.name} and I'm {props.age} years old.</p>
+            <p>{props.children}</p>
+            <input
+                type="text"
+                onChange={props.changed}
+                value={props.name}
+            />
+        </div>
     );
 };
 
